58da180a0bc0f51d8a74e8a6f49c0a035a4e815f
MissoffDev <129719599+azohan79@users.noreply.github.com>
2025-12-02 11:14:57 +0100

    Update urls.py

--- corelink/urls.py
+++ corelink/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from coreconfig import views
 
-app_name = "corelink"
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIR)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

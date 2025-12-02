0f380e20b6c0c35fd7a4da2da67d1ef1d90c335d
MissoffDev <129719599+azohan79@users.noreply.github.com>
2025-12-02 11:09:39 +0100

    Update views.py

--- coreconfig/views.py
+++ coreconfig/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """
+    Главная страница CoreLink.
+    Шаблон: templates/corelink/index.html
+    """
+    return render(request, "corelink/index.html")
+
+
